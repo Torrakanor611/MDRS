5da0d4e2c1b4d94c0f4d208f84210e943ad6c8c8
joao.gameiro@ua.pt <joao.gameiro@ua.pt>
2021-12-09 23:12:39 +0000

    1d com vários valores de C

--- projecto1/ex1d.m
+++ projecto1/ex1d.m
@@ -1,6 +1,6 @@
 %P = 10000;   %Critério de paragem
 l = 1800;    %Packet rate
-C = 10;      %Capacidade do link
+C = [10, 20, 30, 40];      %Capacidade do link
 f = 1000000; %Tamanho da queue
 
 numelems = (109 - 65 + 1) + (1517 - 111 + 1);
@@ -13,29 +13,32 @@ b = 111:1517;
 b = b*(probcadaelem/100);
 
 numMedioBytes = 0.19*64 + 0.23*110 + 0.17*1518 + sum(a) + sum(b);
-tmpMedio = (numMedioBytes * 8) / (C*10^6);
 
 bytes = 64:1518;
-S = (bytes .* 8)./(C*10^6);
-S2 = (bytes .* 8)./(C*10^6);
 
-for i = 1:length(bytes)
-    if i == 1
-        S(i) = S(i)*0.19;
-        S2(i) = S2(i)^2*0.19;
-    elseif i == 110-64+1
-        S(i) = S(i)*0.23;
-        S2(i) = S2(i)^2*0.23;
-    elseif i == 1518-64+1
-        S(i) = S(i)*0.17;
-        S2(i) = S2(i)^2*0.17;
-    else
-        S(i) = S(i)*(probcadaelem/100);
-        S2(i) = S2(i)^2*(probcadaelem/100);
+for j = 1:length(C)
+    S = (bytes .* 8)./(C(j)*10^6);
+    S2 = (bytes .* 8)./(C(j)*10^6);
+    for i = 1:length(bytes)
+        if i == 1
+            S(i) = S(i)*0.19;
+            S2(i) = S2(i)^2*0.19;
+        elseif i == 110-64+1
+            S(i) = S(i)*0.23;
+            S2(i) = S2(i)^2*0.23;
+        elseif i == 1518-64+1
+            S(i) = S(i)*0.17;
+            S2(i) = S2(i)^2*0.17;
+        else
+            S(i) = S(i)*(probcadaelem/100);
+            S2(i) = S2(i)^2*(probcadaelem/100);
+        end
     end
+    
+    ES = sum(S);
+    ES2 = sum(S2);
+    
+    W = ((l*ES2/(2*(1-l*(ES)))) + ES)*10^3;
+    fprintf("Link Capacity: %2d Mbps -> ", C(j));
+    fprintf("Average Packet Delay:  %.2e ms\n", W);
 end
-
-ES = sum(S);
-ES2 = sum(S2);
-
-W = ((l*ES2/(2*(1-l*(ES)))) + ES)*10^3
